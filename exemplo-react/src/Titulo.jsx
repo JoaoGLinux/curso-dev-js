2e7156cabe83bea9ae07bea12f8384e39caefd07
Joao Vitor Parisi Martins Garcia <Joao Vitor Parisi Martins Garcia>
2025-10-25 11:50:57 -0300

    Ultimo git COMMIT!

---
+++ exemplo-react/src/Titulo.jsx
@@ -0,0 +1,7 @@
+export default function Titulo({titulo = "Titulo da Página"}){
+    return (
+        <h1>{titulo}</h1>
+    )
+}
+
+//export default Titulo;
---
+++ exemplo-react/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Titulo from './Titulo.jsx'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Titulo />
+    <Titulo titulo = 'Outro Título'/>
+    <App />
+  </StrictMode>,
+)
